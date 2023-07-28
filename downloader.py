1e81b56eb0f8ec33eed9193ee38b167967f30b6f
david <david.sat@campus.technion.ac.il>
2023-07-28 15:23:55 +0300

    now with extra downloading

--- downloader.py
+++ downloader.py
@@ -1,2 +1,20 @@
+from pytube import YouTube
+import sys
+
+
+def Download(link):
+    ytObj = YouTube(link)
+
+    ytObj = ytObj.streams.get_highest_resolution()
+    try:
+        ytObj.download()
+    except ():
+        print("downloader alert")
+    print("get scraped")
+
+
 if __name__ == "__main__":
-    print("youtube")
+
+    print(str(sys.argv))
+    link = str(sys.argv[1])
+    Download(link)
