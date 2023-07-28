64f95128841799ceafa187e26d0552f747191d44
david <david.sat@campus.technion.ac.il>
2023-07-28 15:37:48 +0300

    i am incomplete

--- downloader.py
+++ downloader.py
@@ -13,8 +13,21 @@ def Download(link):
     print("get scraped")
 
 
-if __name__ == "__main__":
+def scrape_playlist_link():
+    print("i am incomplete")
 
+
+def scrape_vids_from_url():
+    print("i am incomplete")
+
+
+if __name__ == "__main__":
+    if str(sys.argv[1]) == "playlist":
+        playlistName = str(sys.argv[2])
+        playlist = scrape_playlist_link(playlistName)
+        videoList = scrape_vids_from_url(playlist)
+        for video in videoList:
+            Download(video)
     print(str(sys.argv))
     link = str(sys.argv[1])
     Download(link)
