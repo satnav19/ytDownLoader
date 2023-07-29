fc9c32980fbd6273466ba4ebce2d0189799d324b
david <david.sat@campus.technion.ac.il>
2023-07-29 17:54:54 +0300

    video and playlist, also -h

--- downloader.py
+++ downloader.py
@@ -1,10 +1,12 @@
 from pytube import YouTube
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 
 def Download(link):
     ytObj = YouTube(link)
-
+    print(f"downloading video from url: {link}")
     ytObj = ytObj.streams.get_highest_resolution()
     try:
         ytObj.download()
@@ -13,21 +15,53 @@ def Download(link):
     print("get scraped")
 
 
-def scrape_playlist_link():
-    print("i am incomplete")
+def helpmessage():
+    print(" Welcome to youtube downloader")
+    print("""
+ If you are trying to download a single video,
+ run the script with the first argument being video ,
+ and the second one being the link.
+ If you are trying to download a whole playlist,
+ the first argument should be playlist ,
+ the second should be the playlist link,
+ and the third one should be the number of videos in the playlist
+ (or the first N videos to be downloaded).
+ make sure it is either public or unlisted , before using this script.
+ to display this message, use -h""")
 
 
-def scrape_vids_from_url():
-    print("i am incomplete")
+def scrape_vids_from_url(url, num):
+    content = ""
+    vids = []
+    page = requests.get(url)
+    content = str(BeautifulSoup(page.content, "html.parser"))
+    start = 0
+    print("alive")
+    for i in range(num):
+        content = content[start:]
+        pos = content.find("watch?v=")
+        end = content[pos:].find("\\")
+        sliced = content[pos:pos + end]
+        result = f"https://www.youtube.com/{sliced}"
+        vids.append(result)
+        start = pos + end
+    print(vids)
+    return vids
 
 
 if __name__ == "__main__":
+    if (sys.argv.__len__() == 1):
+        print("insufficient arguments , try -h for help")
+        exit()
     if str(sys.argv[1]) == "playlist":
-        playlistName = str(sys.argv[2])
-        playlist = scrape_playlist_link(playlistName)
-        videoList = scrape_vids_from_url(playlist)
+        playlist = str(sys.argv[2])
+        num = int(sys.argv[3])
+        videoList = scrape_vids_from_url(playlist, num)
         for video in videoList:
             Download(video)
-    print(str(sys.argv))
-    link = str(sys.argv[1])
-    Download(link)
+        exit()
+    if str(sys.argv[1]) == "video":
+        link = str(sys.argv[2])
+        Download(link)
+    if str(sys.argv[1]) == "-h":
+        helpmessage()
