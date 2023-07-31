8e75fd63d6a106748c1f99713bdd6dd7237bb242
david <david.sat@campus.technion.ac.il>
2023-07-31 20:07:47 +0300

    Audio, folders, help,script, playlists finished

---
+++ README.txt
@@ -0,0 +1,19 @@
+ Welcome to my youtube downloader !
+ I’ve added a shebang to the top of the .py file , so if you aloow it to execute    using chmod +x downloader.py , you can run it directly.
+ All required Python libraries are in requirements.txt , but not all of those are      needed :) 
+ If you are trying to download a single video,
+ run the script with the first argument being video ,
+ and the second one being the link.
+ To download a single audio-only file(from a video) ,
+ the first argument should be audio, and the second one should be the link.
+ If you are trying to download a whole playlist,
+ the first argument should be playlist ,
+ the second should be the playlist link,
+ and the third one should be the number of videos you wish to download.
+ If a third argument isn't provided,the entire playlist will be downloaded.
+ Make sure it is either public or unlisted , before using this script.
+ To download only the audio portions from a playlist , use tracklist instead,
+ with the rest of the arguments being the same as with a regular playlist.
+ To display this message, use -h
+
+
--- downloader.py
+++ downloader.py
@@ -1,17 +1,29 @@
-from pytube import YouTube
+#!/usr/bin/env python
+from pytube import YouTube, Playlist
 import sys
 import requests
 from bs4 import BeautifulSoup
+import os
 
 
-def Download(link):
+def Download(link, isAudio, folderName="Downloads"):
     ytObj = YouTube(link)
-    print(f"downloading video from url: {link}")
-    ytObj = ytObj.streams.get_highest_resolution()
-    try:
-        ytObj.download()
-    except ():
-        print("downloader alert")
+    if isAudio is False:
+        print(f"downloading video from url: {link}")
+        ytObj = ytObj.streams.get_highest_resolution()
+        try:
+            ytObj.download(output_path=folderName)
+        except Exception:
+            print("downloader alert")
+    else:
+        print(f"downloading audio from url: {link}")
+        ytObj = ytObj.streams.get_audio_only()
+        try:
+            path = ytObj.download(output_path=folderName)
+            newName = path
+            os.rename(path, newName)
+        except Exception:
+            print("downloader alert")
     print("get scraped")
 
 
@@ -21,13 +33,17 @@ def helpmessage():
  If you are trying to download a single video,
  run the script with the first argument being video ,
  and the second one being the link.
+ To download a single audio-only file(from a video) ,
+ the first argument should be audio, and the second one should be the link.
  If you are trying to download a whole playlist,
  the first argument should be playlist ,
  the second should be the playlist link,
- and the third one should be the number of videos in the playlist
- (or the first N videos to be downloaded).
- make sure it is either public or unlisted , before using this script.
- to display this message, use -h""")
+ and the third one should be the number of videos you wish to download.
+ If a third argument isn't provided,the entire playlist will be downloaded.
+ Make sure it is either public or unlisted , before using this script.
+ To download only the audio portions from a playlist , use tracklist instead,
+ with the rest of the arguments being the same as with a regular playlist.
+ To display this message, use -h""")
 
 
 def scrape_vids_from_url(url, num):
@@ -36,7 +52,6 @@ def scrape_vids_from_url(url, num):
     page = requests.get(url)
     content = str(BeautifulSoup(page.content, "html.parser"))
     start = 0
-    print("alive")
     for i in range(num):
         content = content[start:]
         pos = content.find("watch?v=")
@@ -45,7 +60,6 @@ def scrape_vids_from_url(url, num):
         result = f"https://www.youtube.com/{sliced}"
         vids.append(result)
         start = pos + end
-    print(vids)
     return vids
 
 
@@ -55,13 +69,47 @@ if __name__ == "__main__":
         exit()
     if str(sys.argv[1]) == "playlist":
         playlist = str(sys.argv[2])
-        num = int(sys.argv[3])
+        plobject = Playlist(playlist)
+        playlistName = plobject.title
+        playlistName = os.getcwd() + f"/{playlistName}"
+        try:
+            os.mkdir(playlistName)
+        except Exception:
+            pass
+        num = plobject.length
+        if len(sys.argv) == 4:
+            num = int(sys.argv[3])
         videoList = scrape_vids_from_url(playlist, num)
         for video in videoList:
-            Download(video)
+            Download(video, False, playlistName)
         exit()
+    if str(sys.argv[1]) == "tracklist":
+        playlist = str(sys.argv[2])
+        plobject = Playlist(playlist)
+        playlistName = plobject.title
+        playlistName = os.getcwd() + f"/{playlistName}"
+        try:
+            os.mkdir(playlistName)
+        except Exception:
+            pass
+        num = plobject.length
+        if len(sys.argv) == 4:
+            num = int(sys.argv[3])
+        audioList = scrape_vids_from_url(playlist, num)
+        for track in audioList:
+            Download(track, True, playlistName)
+        exit()
+    try:
+        os.mkdir("Downloads")
+    except Exception:
+        pass
     if str(sys.argv[1]) == "video":
         link = str(sys.argv[2])
-        Download(link)
+        Download(link, False)
+        exit()
+    if str(sys.argv[1]) == "audio":
+        link = str(sys.argv[2])
+        Download(link, True)
+        exit()
     if str(sys.argv[1]) == "-h":
         helpmessage()
